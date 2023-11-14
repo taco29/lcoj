6e4648e63012e2ddc77bf32afc62b7e6ca3bf6eb
taco29 <huyvu292007@gmail.com>
2023-11-14 20:53:20 +0700

    lcoj cpp

---
+++ CGAME.cpp
@@ -0,0 +1,21 @@
+//i miss her
+#include <bits/stdc++.h>
+#define f() {ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);}
+#define ll long long
+#define PI acos(-1)
+#define tron(x) setprecision(x) << fixed
+#define FOR for(int i = 0; i < n; i++)
+#define enl "\n"
+using namespace std;
+
+int main(){
+    f();
+    ll a, b;
+    cin >> a >> b;
+    if(a > b){
+        cout << "1";
+    }else{
+        cout << "0";
+    }
+    return 0;
+}
