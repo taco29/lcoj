e17cef718eccf55415c2407d4f86699cea11fd40
taco29 <huyvu292007@gmail.com>
2023-11-14 20:46:33 +0700

    lcoj cpp

---
+++ KTMOI.cpp
@@ -0,0 +1,17 @@
+//i miss her
+#include <bits/stdc++.h>
+#define f() {ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);}
+#define ll long long
+#define PI acos(-1)
+#define tron(x) setprecision(x) << fixed
+#define FOR for(int i = 0; i < n; i++)
+#define enl "\n"
+using namespace std;
+
+int main(){
+    char a;
+    cin >> a;
+    a = tolower(a);
+    cout << a;
+    return 0;
+}
